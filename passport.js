dfb4888ffa023a19cb0dc4f120070648fe918fe9
Mathias Gautier <mths.gautier@gmail.com>
2020-08-23 20:09:15 +0200

    jwt

--- passport.js
+++ passport.js
@@ -8,24 +8,24 @@ require("dotenv").config();
 const cookieExtractor = req =>{
     let token = null;
     if(req && req.cookies){
-        token = req.cookies['access_token']
+        token = req.cookies['jwt']
     }
-    return token
+    return token;
 }
 
 
 // authorization
 passport.use(new JwtStrategy({
-    jwtFromRequest : ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    jwtFromRequest : cookieExtractor,
     secretOrKey: process.env.JWT_SECRET,
 },(payload, done)=>{
     User.findById({_id : payload.sub},(err,user)=>{
         if(err)
-        return done(err, false);
+            return done(err, false);
         if (user)
-        return done(null,user);
+            return done(null,user);
         else
-        return done(null, false);
+            return done(null, false);
     });
 }));
 
